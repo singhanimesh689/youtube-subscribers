553106e734f31f0d696c253d59729c5acaf0bd86
Animesh <singhanimesh689@gmail.com>
2024-07-11 20:17:41 +0530

    changed the url for the server in swagger api documentation

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const options = {
         },
         servers : [
             {
-                url : "http://localhost:3000",
+                url : "https://youtube-subscribers-ij8s.onrender.com",
             },
         ],
     },
